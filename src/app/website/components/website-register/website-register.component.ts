f8c4bf313ee840bb50274409ca77a907f13dd2c4
Stephen Wright <sjw948@gmail.com>
2017-07-13 21:54:19 +0100

    added flash messages + first / last name  + email validation

--- src/app/website/components/website-register/website-register.component.ts
+++ src/app/website/components/website-register/website-register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WebsiteApiService } from '../../website-api.service';
+import { FlashMessagesService } from "angular2-flash-messages"
 
 @Component({
   selector: 'app-website-register',
@@ -10,13 +11,15 @@ export class WebsiteRegisterComponent implements OnInit {
 
   constructor(
     private apiService: WebsiteApiService,
+    private flashMessage: FlashMessagesService
   ) { }
 
   ngOnInit() {
   }
 
   register(userObject) {
-    console.log(userObject);
+    this.validateName(userObject.firstName, userObject.lastName)
+    this.validateEmail(userObject.email)
     this.apiService.register(userObject)
     .subscribe(res => {
       console.log(res)
@@ -24,6 +27,26 @@ export class WebsiteRegisterComponent implements OnInit {
     error => {
       console.log(error)
     })
+  }
+
+  validateName(firstName, lastName) {
+    if(firstName.length <= 1 || lastName.length <= 1) {
+
+      return this.flashError("First & Last name must be longer than 1 character");
+    }
+  }
+
+  validateEmail(email) {
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(regex.test(email)) {
+      return;
+    } else {
+      return this.flashError("Invalid Email address");
+    }
+  }
+
+  flashError(error) {
+    return this.flashMessage.show(error, {cssClass: "flash-failure", timeout: 2000})
   }
 
 }
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -2,23 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { FlashMessagesModule } from "angular2-flash-messages"
+
+// components
 import { WebsiteViewComponent } from './components/website-view/website-view.component';
 import { WebsiteRegisterComponent } from './components/website-register/website-register.component';
 import { WebsiteLoginComponent } from './components/website-login/website-login.component';
 import { WebsiteNavbarComponent } from './components/website-navbar/website-navbar.component';
 import { WebsiteHomeComponent } from './components/website-home/website-home.component';
 
+// routes
 import { AppRoutes } from '../app.routes';
 
+// services
 import { WebsiteApiService } from './website-api.service';
 
 @NgModule({
-  declarations: [WebsiteViewComponent, WebsiteRegisterComponent, WebsiteLoginComponent, WebsiteNavbarComponent, WebsiteHomeComponent],
+  declarations: [
+    WebsiteViewComponent,
+    WebsiteRegisterComponent,
+    WebsiteLoginComponent,
+    WebsiteNavbarComponent,
+    WebsiteHomeComponent
+  ],
   imports: [
     BrowserModule,
+    FlashMessagesModule,
     FormsModule,
     HttpModule,
-    AppRoutes
+    AppRoutes,
   ],
   providers: [WebsiteApiService],
   bootstrap: []
