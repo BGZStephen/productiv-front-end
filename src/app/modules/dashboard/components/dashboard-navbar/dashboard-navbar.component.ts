385385577bcaa63ae79ba73f5c8a98dec19cbe2f
Stephen Wright <sjw948@gmail.com>
2017-07-16 16:58:41 +0100

    added dynamic menu / sub-menu resizing based on user actions / screen sizes

--- src/app/modules/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
+++ src/app/modules/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
@@ -12,4 +12,47 @@ export class DashboardNavbarComponent implements OnInit {
   ngOnInit() {
   }
 
+  activeSubmenu: number = -1;
+  menuVisible = false;
+
+  menuVisibilityToggle() {
+    this.menuVisible = !this.menuVisible;
+  }
+
+  menuVisibilityStyling() {
+    if(screen.width >= 1024) {
+      this.menuVisible = true
+    }
+    if(!this.menuVisible) {
+      return {'visibility': '0', 'opacity': '0'};
+    } else {
+      return {'visibility': '1', 'opacity': '1'};
+    }
+  }
+
+  menuVisibilityOnResize() {
+    if(screen.width >= 1024) {
+      this.menuVisible = true
+    } else {
+      this.menuVisible = false
+    }
+  }
+
+  submenuVisibilityToggle(index) {
+    if(index === this.activeSubmenu) {
+      this.activeSubmenu = -1;
+    } else {
+      this.activeSubmenu = index;
+    }
+  }
+
+  submenuVisibilityStyling(index) {
+    if(index !== this.activeSubmenu) {
+      return {'max-height': '0'}
+    } else {
+      let maxHeight = (document.getElementsByClassName('second-level-menu')[index].children.length * 45) + 'px'
+      return {'max-height': maxHeight}
+    }
+  }
+
 }
